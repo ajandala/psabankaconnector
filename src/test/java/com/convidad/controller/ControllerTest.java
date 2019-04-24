597722c4195b5335c64b973915c6b57760e4449b
Andras Jandala <jandala.andras@gmail.com>
2019-04-24 23:43:24 +0200

    adding test coverage

--- src/test/java/com/convidad/controller/ControllerTest.java
+++ src/test/java/com/convidad/controller/ControllerTest.java
@@ -2,14 +2,15 @@ package com.convidad.controller;
 
 import com.convidad.dto.PaymentDTO;
 import com.convidad.service.PaymentService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.assertj.core.util.Lists;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.mockito.*;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -17,6 +18,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -34,6 +36,9 @@ public class ControllerTest {
     @Mock
     private PaymentService paymentService;
 
+    @Captor
+    public ArgumentCaptor<PaymentDTO> paymentDTOCaptor;
+
     @Before
     public void setup() {
 
@@ -41,19 +46,41 @@ public class ControllerTest {
     }
 
     @Test
-    public void shouldReturnTransactionDetails() throws Exception {
+    public void shouldReturnTransactionDetails_whenGetTransactions() throws Exception {
 
         //GIVEN
         PaymentDTO paymentDTO = givenAPaymentDTO("expectedAccountFrom", "expectedAccountTo");
         Mockito.when(paymentService.getTransactions("accountToTest")).thenReturn(Lists.newArrayList(paymentDTO));
 
         //WHEN
-        ResultActions result = mockMvc.perform(get(BASE_PATH + "//transactions/accountToTest"));
+        ResultActions result = mockMvc.perform(get(BASE_PATH + "/transactions/accountToTest"));
 
         //THEN
         result.andExpect(jsonPath("$[0].accountFrom", is("expectedAccountFrom")));
         result.andExpect(jsonPath("$[0].accountTo", is("expectedAccountTo")));
         result.andExpect(status().isOk());
+
+        Mockito.verify(paymentService).getTransactions("accountToTest");
+    }
+
+    @Test
+    public void shouldCallPaymentServiceWithCorrectParams_whenPayment() throws Exception {
+
+        //GIVEN
+        PaymentDTO paymentDTO = givenAPaymentDTO("expectedAccountFrom", "expectedAccountTo");
+        ObjectMapper mapper = new ObjectMapper();
+
+        String jsonContent = mapper.writeValueAsString(paymentDTO);
+
+        //WHEN
+        ResultActions result = mockMvc.perform(post(BASE_PATH + "/payment").contentType(MediaType.APPLICATION_JSON_VALUE).content(jsonContent));
+
+        //THEN
+        result.andExpect(status().isOk());
+
+        Mockito.verify(paymentService).payment(paymentDTOCaptor.capture());
+        Assert.assertEquals("expectedAccountFrom", paymentDTOCaptor.getValue().getAccountFrom());
+        Assert.assertEquals("expectedAccountTo", paymentDTOCaptor.getValue().getAccountTo());
     }
 
     private PaymentDTO givenAPaymentDTO(String accountFrom, String accountTo) {
